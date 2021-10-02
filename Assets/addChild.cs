c507653591e8fbd9de1e6764f5d3395be5d27581
PonzioDelCarusel <gabriel_corbani@hotmail.com>
2021-10-02 02:08:41 -0300

    Attach plataforma

---
+++ Assets/addChild.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addChild : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.transform.parent = transform;
+        }
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.transform.parent = null;
+        }
+    }
+}
